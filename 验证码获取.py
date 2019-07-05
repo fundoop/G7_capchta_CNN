025100dde8561c4477acb229ffac217b0b3450ed
White_fang <white_fang@126.com>
2019-07-05 10:14:14 +0800

    Update 验证码获取.py

--- 验证码获取.py
+++ 验证码获取.py
@@ -20,7 +20,6 @@ headdic={
 'Host': 'hosturl',
 'Upgrade-Insecure-Requests': '1',
 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-head=urlencode(headdic).encode('utf-8')
 url='url'
 data=[]
 cookie=hc.MozillaCookieJar()
